c4a31c8b723abde17009498e975f9be38cd1e3e2
David Sundqvist <darhagon@gmail.com>
2021-04-30 13:50:14 +0200

    attempted parcel react version

---
+++ src/options/options.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Options() {
+  return (
+    <form>
+      <h1>Options</h1>
+      {/* options goes here... */}
+    </form>
+  );
+}
+
+ReactDOM.render(<Options/>, document.getElementById("root"));
