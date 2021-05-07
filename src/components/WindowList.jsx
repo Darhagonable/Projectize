519583d2a8a9de7783e2fbd83a9888e382d686a4
David Sundqvist <darhagon@gmail.com>
2021-05-07 08:58:46 +0200

    fixed error when failed to capture thumbnail

--- src/components/WindowList.jsx
+++ src/components/WindowList.jsx
@@ -3,11 +3,12 @@ import browser from "webextension-polyfill";
 
 export default function WindowList() {
   const [windows, setWindows] = React.useState([])
-  
+
   useEffect(async () => {
     let tempWindows = await browser.windows.getAll({populate:true})
     for(const window of tempWindows) {
-      window.thumbnail = await browser.tabs.captureVisibleTab(window.id, {quality: 12})
+      let capturing = browser.tabs.captureVisibleTab(window.id, {quality: 12});
+      await capturing.then((imageUri) => window.thumbnail = imageUri, () => window.thumbnail = "noImg");
     }
     setWindows(tempWindows)
   }, [])
@@ -16,7 +17,7 @@ export default function WindowList() {
     <ul>
       {windows.map((window) => (
         <li key={window.id}>text {console.log(window)}
-          <img src={window.thumbnail} alt="No Thumbnail Found" height="115px" width="226px"/>
+          <img src={window.thumbnail} alt="window thumbnail" height="115px" width="226px"/>
         </li>
       ))}
     </ul>
