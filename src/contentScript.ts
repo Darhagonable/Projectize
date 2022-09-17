6eb7fd354e0846d64f999afe54d71e777e269701
David Sundqvist <darhagon@gmail.com>
2022-09-17 02:46:51 +0200

    toggle panel

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -5,7 +5,15 @@ class ProjectizePanel extends HTMLElement {
     iframe.style.border = "none";
     iframe.style.position = "fixed";
     iframe.style.zIndex = "2147483647";
+    iframe.style.visibility = "hidden";
     this.append(iframe);
+  }
+
+  toggle() {
+    const iframe = this.querySelector("iframe");
+    if(!iframe)
+      throw new Error("Iframe wasn't found");
+    iframe.style.visibility === "hidden" ? iframe.style.visibility = "visible" : iframe.style.visibility = "hidden";
   }
 }
 
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
+  // eslint-disable-next-line no-console
   console.log("Extenstion is running (onInstalled triggered)");
 });
 
@@ -6,6 +7,11 @@ chrome.action.onClicked.addListener((tab) => {
   if(!tab.id) return;
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: () => console.log("Yoooo")
+    func: () => {
+      const panel = document.querySelector<ProjectizePanel>("projectize-panel");
+      if(!panel)
+        throw new Error("Panel wasn't found");
+      panel.toggle();
+    }
   });
 });
