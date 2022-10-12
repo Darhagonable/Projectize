76ba5b3d926ae592b157988b51d94de2502cf3c4
David Sundqvist <darhagon@gmail.com>
2022-10-12 21:48:04 +0200

    options page design update

--- src/options/Options/Options.tsx
+++ src/options/Options/Options.tsx
@@ -1,28 +1,44 @@
+import { ToggleButton, ToggleButtonGroup, FormLabel, Typography } from "@mui/material";
 import { useChromeStorageSync } from "use-chrome-storage";
-
-const positions: Array<Position> = ["Top", "Bottom", "Right", "Left"];
+import WebAssetIcon from "@mui/icons-material/WebAsset";
+import { Container } from "@mui/system";
 
 export default function Options() {
-  const [currentPosition, setPosition] = useChromeStorageSync<Position>("position");
+  const [position, setPosition] = useChromeStorageSync<Position>("position");
 
-  function handleButtonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    const newPosition = event.currentTarget.dataset.position as Position;
+  function handleButtonClick(_event: React.MouseEvent, newPosition: Position) {
     setPosition(newPosition);
   }
 
   return (
-    <div>
-      <h1>Options</h1>
-      {positions.map((position) => (
-        <button
-          key={position}
-          data-position={position}
-          onClick={handleButtonClick}
-          style={position === currentPosition ? {border: "2px solid blue"} : undefined}
-        >
-          {position}
-        </button>
-      ))}
-    </div>
+    <Container>
+      <Typography variant="h4" gutterBottom mt={1}>Options</Typography>
+
+      <Typography variant="h6" gutterBottom mt={1} color="text.secondary">
+        Placement
+      </Typography>
+      <ToggleButtonGroup
+        exclusive={true}
+        value={position}
+        onChange={handleButtonClick}
+      >
+        <ToggleButton value="Top" sx={{display: "flex", flexDirection: "column", px: 3}}>
+          <WebAssetIcon sx={{transform: "rotate(0deg)", fontSize: 42}}/>
+          <FormLabel sx={{fontSize: 14, mb: -.5}}>Top</FormLabel>
+        </ToggleButton>
+        <ToggleButton value="Bottom" sx={{display: "flex", flexDirection: "column", px: 3}}>
+          <WebAssetIcon sx={{transform: "rotate(180deg)", fontSize: 42}}/>
+          <FormLabel sx={{fontSize: 14, mb: -.5}}>Bottom</FormLabel>
+        </ToggleButton>
+        <ToggleButton value="Right" sx={{display: "flex", flexDirection: "column", px: 3}}>
+          <WebAssetIcon sx={{transform: "rotate(90deg)", fontSize: 42}}/>
+          <FormLabel sx={{fontSize: 14, mb: -.5}}>Right</FormLabel>
+        </ToggleButton>
+        <ToggleButton value="Left" sx={{display: "flex", flexDirection: "column", px: 3}}>
+          <WebAssetIcon sx={{transform: "rotate(270deg)", fontSize: 42}}/>
+          <FormLabel sx={{fontSize: 14, mb: -.5}}>Left</FormLabel>
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Container>
   );
 }
